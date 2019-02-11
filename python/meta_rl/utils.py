3648b8f33945c51095da2a7abf43ade7229b3d80
mtrazzi <mtrazzi@student.42.fr>
2019-02-11 12:28:53 +0100

    added two-step-task code and divided it into classes

---
+++ python/meta_rl/utils.py
@@ -0,0 +1,25 @@
+import threading
+import multiprocessing
+import numpy as np
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
+import scipy.signal
+
+def update_target_graph(from_scope,to_scope):
+    from_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, from_scope)
+    to_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, to_scope)
+
+    op_holder = []
+    for from_var,to_var in zip(from_vars,to_vars):
+        op_holder.append(to_var.assign(from_var))
+    return op_holder
+
+def discount(x, gamma):
+    return scipy.signal.lfilter([1], [1, -gamma], x[::-1], axis=0)[::-1]
+
+def normalized_columns_initializer(std=1.0):
+    def _initializer(shape, dtype=None, partition_info=None):
+        out = np.random.randn(*shape).astype(np.float32)
+        out *= std / np.sqrt(np.square(out).sum(axis=0, keepdims=True))
+        return tf.constant(out)
+    return _initializer
